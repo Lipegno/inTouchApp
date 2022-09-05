db5d8eafbd863d5b910f07ef5337af96b66d6cc8
Irina <irinavasilitav@gmail.com>
2022-09-05 18:49:59 +0100

    Add method to delete the pending connection

--- app/src/main/java/com/example/intouch/dao/DAOPendingConnection.java
+++ app/src/main/java/com/example/intouch/dao/DAOPendingConnection.java
@@ -1,9 +1,14 @@
 package com.example.intouch.dao;
 
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 
 import com.example.intouch.db.PendingConnection;
 import com.example.intouch.helpers.Callback;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -27,27 +32,65 @@ public class DAOPendingConnection {
         return instance;
     }
 
-    public DAOPendingConnection(){
+    public DAOPendingConnection() {
         FirebaseDatabase db = FirebaseDatabase.getInstance();
 
         databaseReference = db.getReference(PendingConnection.class.getSimpleName());
     }
 
-    public Task<Void> add(PendingConnection pendingConnection){
+    public Task<Void> add(PendingConnection pendingConnection) {
         return databaseReference.push().setValue(pendingConnection);
     }
 
-    public void getPendingConnectionByAUserUid(String userUid, final Callback<PendingConnection> onReceived, final Callback onFailed){
+    public void getPendingConnectionByAUserUid(String userUid, final Callback<PendingConnection> onReceived, final Callback onFailed) {
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Iterable<DataSnapshot> pendingConnectionChildren = dataSnapshot.getChildren();
 
-                for (DataSnapshot connection: pendingConnectionChildren) {
+                for (DataSnapshot connection : pendingConnectionChildren) {
                     PendingConnection pc = connection.getValue(PendingConnection.class);
 
-                    if(pc.receiverUID.equalsIgnoreCase(userUid) || pc.senderUID.equalsIgnoreCase(userUid)){
+                    if (pc.receiverUID.equalsIgnoreCase(userUid) || pc.senderUID.equalsIgnoreCase(userUid)) {
                         onReceived.execute(pc);
+                        return;
+                    }
+                }
+
+                onFailed.execute(null);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void deletePendingConnectionByAUsersUid(String senderUid, String receiverUid, final Callback onDeleted, final Callback onFailed) {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> pendingConnectionChildren = dataSnapshot.getChildren();
+
+                for (DataSnapshot connection : pendingConnectionChildren) {
+                    PendingConnection pc = connection.getValue(PendingConnection.class);
+
+                    if (pc.receiverUID.equalsIgnoreCase(receiverUid) || pc.senderUID.equalsIgnoreCase(senderUid)) {
+                        connection.getRef().removeValue()
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        onDeleted.execute(null);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        onFailed.execute(null);
+                                    }
+                                });
+
                         return;
                     }
                 }
