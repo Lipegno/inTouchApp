387cca15053de915e0155c3f5594634e8f785e0f
Irina <irinavasilitav@gmail.com>
2022-09-05 19:01:58 +0100

    Add the "notified" field
    Checks if the sender was notified about the request being accepted.

--- app/src/main/java/com/example/intouch/db/Connection.java
+++ app/src/main/java/com/example/intouch/db/Connection.java
@@ -1,17 +1,25 @@
 package com.example.intouch.db;
 
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Connection {
     public UserInfo firstUser;
     public UserInfo secondUser;
+    public int notified;
 
-    public Connection(){
+    public Connection() {
         firstUser = new UserInfo();
         secondUser = new UserInfo();
+        notified = 0;
     }
 
-    public Connection(UserInfo first, UserInfo second){
+    public Connection(UserInfo first, UserInfo second) {
         this.firstUser = first;
         this.secondUser = second;
+        this.notified = 0;
     }
 
     public UserInfo getFirstUser() {
@@ -28,5 +36,23 @@ public class Connection {
 
     public void setSecondUser(UserInfo secondUser) {
         this.secondUser = secondUser;
+    }
+
+    public int getNotified() {
+        return notified;
+    }
+
+    public void setNotified(int notified) {
+        this.notified = notified;
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("firstUser", firstUser);
+        result.put("secondUser", secondUser);
+        result.put("notified", notified);
+
+        return result;
     }
 }
