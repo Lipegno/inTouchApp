39318fbbf6ffc6b1abbc325fe57eda631fec82b6
Irina <irinavasilitav@gmail.com>
2022-09-05 19:00:52 +0100

    Add notifyConnection() method
    Method that updates the "notified" field after the sender sees for the first time that the request was accepted.

--- app/src/main/java/com/example/intouch/dao/DAOConnection.java
+++ app/src/main/java/com/example/intouch/dao/DAOConnection.java
@@ -4,12 +4,16 @@ import androidx.annotation.NonNull;
 
 import com.example.intouch.db.Connection;
 import com.example.intouch.helpers.Callback;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class DAOConnection {
     private static volatile DAOConnection instance;
@@ -27,26 +31,26 @@ public class DAOConnection {
         return instance;
     }
 
-    private DAOConnection(){
+    private DAOConnection() {
         FirebaseDatabase db = FirebaseDatabase.getInstance();
 
         databaseReference = db.getReference(Connection.class.getSimpleName());
     }
 
-    public Task<Void> add(Connection connection){
+    public Task<Void> add(Connection connection) {
         return databaseReference.push().setValue(connection);
     }
 
-    public void getConnectionByAUserUid(String userUid, final Callback<Connection> onReceived, final Callback onFailed){
+    public void getConnectionByAUserUid(String userUid, final Callback<Connection> onReceived, final Callback onFailed) {
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Iterable<DataSnapshot> connectionChildren = dataSnapshot.getChildren();
 
-                for (DataSnapshot connection: connectionChildren) {
+                for (DataSnapshot connection : connectionChildren) {
                     Connection c = connection.getValue(Connection.class);
 
-                    if(c.firstUser.userUID.equalsIgnoreCase(userUid) || c.secondUser.userUID.equalsIgnoreCase(userUid)){
+                    if (c.firstUser.userUID.equalsIgnoreCase(userUid) || c.secondUser.userUID.equalsIgnoreCase(userUid)) {
                         onReceived.execute(c);
                         return;
                     }
@@ -61,4 +65,34 @@ public class DAOConnection {
             }
         });
     }
+
+    public void notifyConnection(Connection connection, final Callback onNotified, final Callback onFailed) {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                connection.notified = 1;
+                Map<String, Object> postValues = connection.toMap();
+                databaseReference.updateChildren(postValues)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                onNotified.execute(null);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                onFailed.execute(null);
+                            }
+                        });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
