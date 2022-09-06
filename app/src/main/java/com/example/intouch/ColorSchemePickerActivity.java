8f2725932d0a1d95e9c8e4575cb552ab0ec56225
Irina <irinavasilitav@gmail.com>
2022-09-06 17:08:37 +0100

    Update ColorSchemePickerActivity

--- app/src/main/java/com/example/intouch/ColorSchemePickerActivity.java
+++ app/src/main/java/com/example/intouch/ColorSchemePickerActivity.java
@@ -3,7 +3,6 @@ package com.example.intouch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,11 +10,14 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.example.intouch.models.ColorScheme;
-import com.example.intouch.models.User;
+import com.example.intouch.dao.DAOUserSettings;
+import com.example.intouch.helpers.Callback;
+import com.example.intouch.models.UserSettings;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.sql.Array;
+import java.util.ArrayList;
 
 public class ColorSchemePickerActivity extends AppCompatActivity {
 
@@ -29,8 +31,10 @@ public class ColorSchemePickerActivity extends AppCompatActivity {
 
     Button buttonOK;
 
-    ColorScheme colorScheme1;
-    ColorScheme colorScheme2;
+    ArrayList<String> colorScheme1;
+    ArrayList<String> colorScheme2;
+
+    ArrayList<String> selectedColorScheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +45,7 @@ public class ColorSchemePickerActivity extends AppCompatActivity {
         radioColorScheme1 = findViewById(R.id.radioColorS1);
         radioColorScheme2 = findViewById(R.id.radioColorS2);
         radioCustomizeColorScheme = findViewById(R.id.radioColorCustomize);
-        buttonOK =  findViewById(R.id.buttonOKColorSchemePicker);
-
-        buttonOK.setEnabled(false);
+        buttonOK = findViewById(R.id.buttonOKColorSchemePicker);
 
         // Disable the customization for now
         radioCustomizeColorScheme.setEnabled(false);
@@ -51,28 +53,28 @@ public class ColorSchemePickerActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
 
-        colorScheme1 = new ColorScheme("#cefad0", "#fff380", "#ff2c2c");
-        colorScheme2 = new ColorScheme("#bfe6ff", "#e88504", "#555555");
-
-        // TO DO:
-        // Set the colors as background to the circles
+        initializeColorSchemes();
 
         radioGroupColorSchemePicker.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
                 // Check which radio button was clicked
-                switch(checkedId) {
+                switch (checkedId) {
                     case R.id.radioColorS1:
                         Toast.makeText(ColorSchemePickerActivity.this, "Color Scheme 1", Toast.LENGTH_SHORT).show();
-                        setColorSchemeSettings(colorScheme1);
+                        selectedColorScheme = colorScheme1;
+                        break;
                     case R.id.radioColorS2:
                         Toast.makeText(ColorSchemePickerActivity.this, "Color Scheme 2", Toast.LENGTH_SHORT).show();
-                        setColorSchemeSettings(colorScheme2);
+                        selectedColorScheme = colorScheme2;
+                        break;
                     case R.id.radioColorCustomize:
                         Toast.makeText(ColorSchemePickerActivity.this, "Color Scheme Customize", Toast.LENGTH_SHORT).show();
                         //customizeColorScheme();
+                        break;
                     default:
                         Toast.makeText(ColorSchemePickerActivity.this, "Default Scheme", Toast.LENGTH_SHORT).show();
+                        selectedColorScheme = colorScheme1;
                         break;
                 }
             }
@@ -81,26 +83,34 @@ public class ColorSchemePickerActivity extends AppCompatActivity {
         buttonOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                saveColorSchemeSettings();
+                redirectToWallpaperSideActivity();
             }
         });
     }
 
-    private void setColorSchemeSettings(ColorScheme colorScheme) {
-        // set the color to user settings
-        buttonOK.setEnabled(true);
-    }
+    private void initializeColorSchemes() {
+        colorScheme1 = new ArrayList<String>();
+        colorScheme1.add("#cefad0");
+        colorScheme1.add("#fff380");
+        colorScheme1.add("#ff2c2c");
 
-    private void saveColorSchemeSettings() {
-        // save settings to DB
-        // on success
-        redirectToWallpaperSideActivity();
+        colorScheme2 = new ArrayList<String>();
+        colorScheme1.add("#bfe6ff");
+        colorScheme1.add("#e88504");
+        colorScheme1.add("#555555");
+
+        // TO DO
+        // Set circles background colors
     }
 
     private void redirectToWallpaperSideActivity() {
         Intent intent = new Intent(this, WallpaperSideActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        Bundle bundle = new Bundle();
+        bundle.putStringArrayList("selectedColorScheme", selectedColorScheme);
+        intent.putExtras(bundle);
+
         startActivity(intent);
     }
 
