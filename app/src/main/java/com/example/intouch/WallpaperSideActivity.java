6ed883ebbe847da4ddfb37de1124af160238eb94
Irina <irinavasilitav@gmail.com>
2022-09-07 14:00:46 +0100

    Update WallpaperSideActivity.java

--- app/src/main/java/com/example/intouch/WallpaperSideActivity.java
+++ app/src/main/java/com/example/intouch/WallpaperSideActivity.java
@@ -2,13 +2,33 @@ package com.example.intouch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.example.intouch.helpers.Callback;
+import com.example.intouch.models.UserSettings;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class WallpaperSideActivity extends AppCompatActivity {
 
+    RadioGroup radioGroupWallpaperPicker;
+    Button buttonOK;
+
     ArrayList<String> selectedColorScheme;
+    String selectedWallpaperSide;
+
+    FirebaseAuth mAuth;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +38,58 @@ public class WallpaperSideActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         selectedColorScheme = bundle.getStringArrayList("selectedColorScheme");
 
+        radioGroupWallpaperPicker = findViewById(R.id.radioGroupWallpaperPicker);
+        buttonOK = findViewById(R.id.buttonOKWallpaperPicker);
 
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        radioGroupWallpaperPicker.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                // Check which radio button was clicked
+                switch (checkedId) {
+                    case R.id.radioWallpaperLeft:
+                        Toast.makeText(WallpaperSideActivity.this, "LeftWallpaper", Toast.LENGTH_SHORT).show();
+                        selectedWallpaperSide = "left";
+                        break;
+                    case R.id.radioWallpaperRight:
+                        Toast.makeText(WallpaperSideActivity.this, "Color Scheme 2", Toast.LENGTH_SHORT).show();
+                        selectedWallpaperSide = "right";
+                        break;
+                    default:
+                        Toast.makeText(WallpaperSideActivity.this, "Default Scheme", Toast.LENGTH_SHORT).show();
+                        selectedWallpaperSide = "right";
+                        break;
+                }
+            }
+        });
+
+        buttonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateUserSettings();
+            }
+        });
+    }
+
+
+    private void updateUserSettings() {
+        SharedPreferences.Editor editor = getSharedPreferences(MainActivity.MY_PREFERENCE, MODE_PRIVATE).edit();
+
+        //Set the selected values
+        Set<String> colorSchemeSet = new HashSet<String>(selectedColorScheme);
+        editor.putStringSet("colorSchemeSet", colorSchemeSet);
+        editor.putString("wallpaperSide", selectedWallpaperSide);
+
+        editor.apply();
+
+        redirectToAllSetActivity();
+    }
+
+    private void redirectToAllSetActivity() {
+        Intent intent = new Intent(this, AllSetActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
