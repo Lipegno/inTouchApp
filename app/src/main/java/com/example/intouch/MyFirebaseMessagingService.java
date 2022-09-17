201db7fb821d1967fff797d23b4ea9b54c0918f4
Irina <irinavasilitav@gmail.com>
2022-09-17 13:09:11 +0100

    Update MyFirebaseMessagingService.java

--- app/src/main/java/com/example/intouch/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/intouch/MyFirebaseMessagingService.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
@@ -29,13 +31,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 "Message Notification",
                 NotificationManager.IMPORTANCE_HIGH);
         getSystemService(NotificationManager.class).createNotificationChannel(channel);
-        Context context;
+        Context context = getApplicationContext();
         Notification.Builder notification = new Notification.Builder(this, CHANNEL_ID)
                 .setContentTitle(title)
                 .setContentText(text)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setAutoCancel(true);
         NotificationManagerCompat.from(this).notify(1, notification.build());
+
+/*
+        private static final int LEFT_BTN = 100;
+        private static final int RIGHT_BTN = 101;
+        private static final int HEART_BTN = 102;
+        private static final int CRY_BTN = 103;
+        private static final int HUG_BTN = 104;
+        private static final int MUSCLE_BTN = 105;
+        private static final int PINK_COLOR_BTN = 106;
+        private static final int GRAY_COLOR_BTN = 107;
+        private static final int GREEN_COLOR_BTN = 108;
+
+ */
+
+        //AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
 
         // TO DO
         // If title is "mood"
