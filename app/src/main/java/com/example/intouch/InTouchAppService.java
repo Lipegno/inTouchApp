007736cb533fd4d8800be6cd64c0401c8b52bff7
Lipegno <filp3q@gmail.com>
2024-07-23 12:31:28 +0100

    final commit

--- app/src/main/java/com/example/intouch/InTouchAppService.java
+++ app/src/main/java/com/example/intouch/InTouchAppService.java
@@ -26,7 +26,6 @@ public class InTouchAppService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-       // throw new UnsupportedOperationException("Not yet implemented");
         return null;
     }
 
